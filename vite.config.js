46210a55381e2534a9293b48084c3ea335fffedc
yartanix <yartanix@gmail.com>
2026-04-15 17:38:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default defineConfig({
+    plugins: [react()],
+})
